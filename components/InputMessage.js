6171dd06385e63f03075e7eba1d8de06bc66b93f
Michele <michele.pioli@studenti.unicam.it>
2022-04-03 17:07:38 +0200

    Send Message feature

--- components/InputMessage.js
+++ components/InputMessage.js
@@ -1,37 +1,62 @@
+import { useState } from "react";
 import { supabase } from "../utils/supabase";
 
 export default function InputMessage() {
-    //const { message } = ??
-    /*
-    function send({ message }) {
-        const { data, error } = await supabase
-            .from("board")
-            .insert([
-                { title: message.title, body: message.body }
-            ])
-        console.log(data, error)
+
+    const [loading, setLoading] = useState(false)
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+
+    const send = async (title, body) => {
+        try {
+            setLoading(true)
+            const { error } = await supabase
+                .from("board")
+                .insert({
+                    title,
+                    body
+                })
+            if (error) throw error
+            alert("Il tuo messaggio è stato mandato!")
+        } catch (error) {
+            alert("Si è verificato un errore, riprova")
+        } finally {
+            setLoading(false)
+        }
     }
-    */
+
     return (
         <div className="rounded-l py-2 px-4 font-medium">
-            <div onSubmit={() => send}>
+            <div>
                 <input className="border-b-2"
                     type="text"
-                    name="title"
-                    id="title"
                     placeholder="Oggetto"
-                />
-                <br></br>
-                <input className="border-b-2"
-                    type="text"
-                    name="body"
-                    id="body"
-                    placeholder="Corpo"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
             </div>
-            <button type="submit" className="py-4 text-sm font-medium">
-                Manda il tuo messaggio
-            </button>
+            <br></br>
+            <div>
+                <input className="border-b-2"
+                    type="text"
+                    placeholder="Corpo"
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+            </div>
+            <br></br>
+            <div>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault()
+                        send(title, body)
+                    }}
+                    className="py-4 text-sm font-medium"
+                    disabled={loading}
+                >
+                    <span>{loading ? "Mandando..." : "Manda il tuo messaggio"}</span>
+                </button>
+            </div>
         </div>
     )
 }
