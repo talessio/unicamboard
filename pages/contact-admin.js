ded6d413da17970bc1cf176e125dba7442dd251e
ElGanzo <lorenzobensong@gmail.com>
2022-04-06 11:22:43 +0200

    DA CORREGGERE form inserimento messaggio
    form inserimento messaggio per contattare l'amministrazione

--- pages/contact-admin.js
+++ pages/contact-admin.js
@@ -1,11 +1,35 @@
 export default function contactAdmin() {
     return (
-
         <div className="rounded-xl m-8 p-8 md:p-4 shadow-sm shadow-slate-300">
             <div className="text-center">
-                <span className="text-lg font-bold">{"Scrivi il tuo messaggio oppure contattaci a prova@example.com"}</span>
+                <span className="text-lg font-bold">{"Inserisci il tuo messaggio oppure contattaci a prova@example.com"}</span>
             </div>
-            //form per inserimento messaggio + bottone invia
+            
+            <div className="rounded-l py-2 px-4 font-medium">
+                <div>
+                    <input className="border-b-2"
+                        type="text"
+                        placeholder="Scrivi qui..."
+                        value={message}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <br></br>
+                <div>
+                    <button
+                        onClick={(e) => {
+                            e.preventDefault()
+                            send(message)
+                        }}
+                        className="py-4 text-sm font-medium"
+                        disabled={loading}>
+                    
+                        <span>{loading ? "Mandando..." : "Manda"}</span>
+                    </button>
+                </div>
+            </div>
+
+
         </div>
 
     );
