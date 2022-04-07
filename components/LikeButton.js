bb1cbc0501fb07b2bd44be15a59de53c03a805ad
Michele <michele.pioli@studenti.unicam.it>
2022-04-07 17:50:16 +0200

    Update LikeButton.js

--- components/LikeButton.js
+++ components/LikeButton.js
@@ -5,11 +5,34 @@ import { supabase } from "../utils/supabase";
 
 export default function LikeButton({ message }) {
     const { user } = useUser();
-    // const id = user ? user.id : null;
-    console.log(message.id);
     const [liked, setLiked] = useState(false)
     const [likeCount, setLikeCount] = useState(message.n_likes) //n_likes ancora non funziona
     const [loading, setLoading] = useState(false)
+
+    const initializeCount = async () => {
+        const postId = message ? message.id : null;
+        const { data, error, count } = await supabase
+            .from("like")
+            .select("id", { count: "exact" })
+            .eq("post_id", postId)
+        setLikeCount(count)
+    }
+
+    const initializeButton = async () => {
+        const id = user ? user.id : null;
+        const postId = message ? message.id : null;
+        const { data, error, count } = await supabase
+            .from("like")
+            .select("profile_id", { count: "exact" })
+            .eq("profile_id", id)
+            .eq("post_id", postId)
+        if (count > 0) {
+            setLiked(true)
+        } 
+        initializeCount()
+    }
+
+    initializeButton()
 
     const handleLike = async () => {
         const id = user ? user.id : null;
