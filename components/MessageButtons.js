6b501a43003731f279f6012d39b128b44f129ddd
Michele <michele.pioli@studenti.unicam.it>
2022-04-11 10:10:19 +0200

    Prettier

--- components/MessageButtons.js
+++ components/MessageButtons.js
@@ -1,8 +1,8 @@
 import {
-    FaRegHeart,
-    FaHeart,
-    FaArrowDown,
-    FaArrowCircleDown
+  FaRegHeart,
+  FaHeart,
+  FaArrowDown,
+  FaArrowCircleDown,
 } from "react-icons/fa";
 import { useState } from "react";
 import { useUser } from "../context/user";
@@ -12,197 +12,193 @@ import { supabase } from "../utils/supabase";
 //create reusable functions (initializeCount, initializeButton)
 
 export const LikeButton = ({ message }) => {
-    const { user } = useUser();
-    const [liked, setLiked] = useState(false)
-    const [likeCount, setLikeCount] = useState(message.n_likes) 
-    const [loading, setLoading] = useState(false)
-    //TODO fix console error: invalid input syntax for type uuid: "null"
+  const { user } = useUser();
+  const [liked, setLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(message.n_likes);
+  const [loading, setLoading] = useState(false);
+  //TODO fix console error: invalid input syntax for type uuid: "null"
 
-    const initializeCount = async () => {
-        const postId = message ? message.id : null;
-        try {
-            const { error, count } = await supabase
-                .from("like")
-                .select("id", { count: "exact" })
-                .eq("post_id", postId)
-            if (error) throw error
-            setLikeCount(count)
-        } catch (error) {
-            console.error(error.message)
-        }
+  const initializeCount = async () => {
+    const postId = message ? message.id : null;
+    try {
+      const { error, count } = await supabase
+        .from("like")
+        .select("id", { count: "exact" })
+        .eq("post_id", postId);
+      if (error) throw error;
+      setLikeCount(count);
+    } catch (error) {
+      console.error(error.message);
     }
+  };
 
-    const initializeButton = async () => {
-        const id = user ? user.id : null;
-        const postId = message ? message.id : null;
-        try {
-            const { error, count } = await supabase
-                .from("like")
-                .select("profile_id", { count: "exact" })
-                .eq("profile_id", id)
-                .eq("post_id", postId)
-            if (error) throw error
-            if (count > 0) {
-                setLiked(true)
-            }
-        } catch (error) {
-            console.error(error.message)
-        }
-        initializeCount()
+  const initializeButton = async () => {
+    const id = user ? user.id : null;
+    const postId = message ? message.id : null;
+    try {
+      const { error, count } = await supabase
+        .from("like")
+        .select("profile_id", { count: "exact" })
+        .eq("profile_id", id)
+        .eq("post_id", postId);
+      if (error) throw error;
+      if (count > 0) {
+        setLiked(true);
+      }
+    } catch (error) {
+      console.error(error.message);
     }
+    initializeCount();
+  };
 
-    initializeButton()
+  initializeButton();
 
-    const handleLike = async () => {
-        const id = user ? user.id : null;
-        const postId = message ? message.id : null;
-        if (!liked) {
-            try {
-                setLoading(true)
-                const { error } = await supabase
-                    .from("like")
-                    .insert({
-                        profile_id: id,
-                        post_id: postId
-                    })
-                if (error) throw error
-            } catch (error) {
-                console.error(error.message)
-            } finally {
-                setLikeCount(likeCount + 1)
-                setLoading(false)
-                setLiked(true)
-            }
-        } else {
-            try {
-                setLoading(true)
-                const { error } = await supabase
-                    .from("like")
-                    .delete()
-                    .match({
-                        profile_id: id,
-                        post_id: postId
-                    })
-                if (error) throw error
-            } catch (error) {
-                console.error(error.message)
-            } finally {
-                setLikeCount(likeCount - 1)
-                setLoading(false)
-                setLiked(false)
-            }
-        }
+  const handleLike = async () => {
+    const id = user ? user.id : null;
+    const postId = message ? message.id : null;
+    if (!liked) {
+      try {
+        setLoading(true);
+        const { error } = await supabase.from("like").insert({
+          profile_id: id,
+          post_id: postId,
+        });
+        if (error) throw error;
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setLikeCount(likeCount + 1);
+        setLoading(false);
+        setLiked(true);
+      }
+    } else {
+      try {
+        setLoading(true);
+        const { error } = await supabase.from("like").delete().match({
+          profile_id: id,
+          post_id: postId,
+        });
+        if (error) throw error;
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setLikeCount(likeCount - 1);
+        setLoading(false);
+        setLiked(false);
+      }
     }
+  };
 
-    return (
-        <div>
-            <button onClick={(e) => {
-                e.preventDefault()
-                handleLike()
-            }}
-                disabled={loading}
-            >
-                <span className="text-pink-400">{liked ? <FaHeart /> : <FaRegHeart />}</span>
-            </button>
-            <p>Likes: {likeCount}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          handleLike();
+        }}
+        disabled={loading}
+      >
+        <span className="text-pink-400">
+          {liked ? <FaHeart /> : <FaRegHeart />}
+        </span>
+      </button>
+      <p>Likes: {likeCount}</p>
+    </div>
+  );
+};
 
 export const DownvoteButton = ({ message }) => {
-    const { user } = useUser();
-    const [downvoted, setDownvoted] = useState(false)
-    const [downvoteCount, setDownvoteCount] = useState(message.n_downvotes) 
-    const [loading, setLoading] = useState(false)
-    //TODO fix console error: invalid input syntax for type uuid: "null"
+  const { user } = useUser();
+  const [downvoted, setDownvoted] = useState(false);
+  const [downvoteCount, setDownvoteCount] = useState(message.n_downvotes);
+  const [loading, setLoading] = useState(false);
+  //TODO fix console error: invalid input syntax for type uuid: "null"
 
-    const initializeCount = async () => {
-        const postId = message ? message.id : null;
-        try {
-            const { error, count } = await supabase
-                .from("downvote")
-                .select("id", { count: "exact" })
-                .eq("post_id", postId)
-            if (error) throw error
-            setDownvoteCount(count)
-        } catch (error) {
-            console.error(error.message)
-        }
+  const initializeCount = async () => {
+    const postId = message ? message.id : null;
+    try {
+      const { error, count } = await supabase
+        .from("downvote")
+        .select("id", { count: "exact" })
+        .eq("post_id", postId);
+      if (error) throw error;
+      setDownvoteCount(count);
+    } catch (error) {
+      console.error(error.message);
     }
+  };
 
-    const initializeButton = async () => {
-        const id = user ? user.id : null;
-        const postId = message ? message.id : null;
-        try {
-            const { error, count } = await supabase
-                .from("downvote")
-                .select("profile_id", { count: "exact" })
-                .eq("profile_id", id)
-                .eq("post_id", postId)
-            if (error) throw error
-            if (count > 0) {
-                setDownvoted(true)
-            }
-        } catch (error) {
-            console.error(error.message)
-        }
-        initializeCount()
+  const initializeButton = async () => {
+    const id = user ? user.id : null;
+    const postId = message ? message.id : null;
+    try {
+      const { error, count } = await supabase
+        .from("downvote")
+        .select("profile_id", { count: "exact" })
+        .eq("profile_id", id)
+        .eq("post_id", postId);
+      if (error) throw error;
+      if (count > 0) {
+        setDownvoted(true);
+      }
+    } catch (error) {
+      console.error(error.message);
     }
+    initializeCount();
+  };
 
-    initializeButton()
+  initializeButton();
 
-    const handleDownvote = async () => {
-        const id = user ? user.id : null;
-        const postId = message ? message.id : null;
-        if (!downvoted) {
-            try {
-                setLoading(true)
-                const { error } = await supabase
-                    .from("downvote")
-                    .insert({
-                        profile_id: id,
-                        post_id: postId
-                    })
-                if (error) throw error
-            } catch (error) {
-                console.error(error.message)
-            } finally {
-                setDownvoteCount(downvoteCount + 1)
-                setLoading(false)
-                setDownvoted(true)
-            }
-        } else {
-            try {
-                setLoading(true)
-                const { error } = await supabase
-                    .from("downvote")
-                    .delete()
-                    .match({
-                        profile_id: id,
-                        post_id: postId
-                    })
-                if (error) throw error
-            } catch (error) {
-                console.error(error.message)
-            } finally {
-                setDownvoteCount(downvoteCount - 1)
-                setLoading(false)
-                setDownvoted(false)
-            }
-        }
+  const handleDownvote = async () => {
+    const id = user ? user.id : null;
+    const postId = message ? message.id : null;
+    if (!downvoted) {
+      try {
+        setLoading(true);
+        const { error } = await supabase.from("downvote").insert({
+          profile_id: id,
+          post_id: postId,
+        });
+        if (error) throw error;
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setDownvoteCount(downvoteCount + 1);
+        setLoading(false);
+        setDownvoted(true);
+      }
+    } else {
+      try {
+        setLoading(true);
+        const { error } = await supabase.from("downvote").delete().match({
+          profile_id: id,
+          post_id: postId,
+        });
+        if (error) throw error;
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setDownvoteCount(downvoteCount - 1);
+        setLoading(false);
+        setDownvoted(false);
+      }
     }
+  };
 
-    return (
-        <div>
-            <button onClick={(e) => {
-                e.preventDefault()
-                handleDownvote()
-            }}
-                disabled={loading}
-            >
-                <span className="text-red-400">{downvoted ? <FaArrowCircleDown /> : <FaArrowDown />}</span>
-            </button>
-            <p>{downvoteCount}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          handleDownvote();
+        }}
+        disabled={loading}
+      >
+        <span className="text-red-400">
+          {downvoted ? <FaArrowCircleDown /> : <FaArrowDown />}
+        </span>
+      </button>
+      <p>{downvoteCount}</p>
+    </div>
+  );
+};
--- pages/board.js
+++ pages/board.js
@@ -1,47 +1,48 @@
-import Header from "../components/Header"
+import Header from "../components/Header";
 import { supabase } from "../utils/supabase";
 //import LikeButton from "../components/LikeButton";
 //import DownvoteButton from "../components/DownvoteButton";
-import {
-    LikeButton,
-    DownvoteButton
-} from "../components/MessageButtons";
+import { LikeButton, DownvoteButton } from "../components/MessageButtons";
 
 export default function board({ messages }) {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <Header />
-            <div className="w-full border-2 border-slate-300 rounded-xl max-w-4xl flex flex-col space-y-4">
-                {messages.map((message) => (
-                    <div className="rounded-xl m-8 p-8 md:p-4 shadow-sm shadow-slate-300" key={message.engagement}>
-                        <div className="flex justify-between h-24 rounded-full mx-auto">
-                            <span className="text-sm">Utente: {message.profile_id}</span>
-                            <span className="text-sm">Interazioni: {message.engagement}</span>
-                        </div>
-                        <div className="text-center">
-                            <span className="text-lg font-bold">{message.title}</span>
-                        </div>
-                        <div className="flex flex-col items-center py-2">
-                            <p className="text-md font-thin">{message.body}</p>
-                        </div>
-                        <div>
-                            <LikeButton message={message} /> <DownvoteButton message={message} />
-                        </div>
-                    </div>
-                ))}
-            </div >
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Header />
+      <div className="w-full border-2 border-slate-300 rounded-xl max-w-4xl flex flex-col space-y-4">
+        {messages.map((message) => (
+          <div
+            className="rounded-xl m-8 p-8 md:p-4 shadow-sm shadow-slate-300"
+            key={message.engagement}
+          >
+            <div className="flex justify-between h-24 rounded-full mx-auto">
+              <span className="text-sm">Utente: {message.profile_id}</span>
+              <span className="text-sm">Interazioni: {message.engagement}</span>
+            </div>
+            <div className="text-center">
+              <span className="text-lg font-bold">{message.title}</span>
+            </div>
+            <div className="flex flex-col items-center py-2">
+              <p className="text-md font-thin">{message.body}</p>
+            </div>
+            <div>
+              <LikeButton message={message} />{" "}
+              <DownvoteButton message={message} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export const getServerSideProps = async () => {
-    const { data: messages } = await supabase.from("post").select("*")
+  const { data: messages } = await supabase.from("post").select("*");
 
-    const sortedMessages = messages.sort((a, b) => a.engagement + b.engagement);
+  const sortedMessages = messages.sort((a, b) => a.engagement + b.engagement);
 
-    return {
-        props: {
-            messages: sortedMessages,
-        }
-    }
-}
+  return {
+    props: {
+      messages: sortedMessages,
+    },
+  };
+};
