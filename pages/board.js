d12ae32edcf80e5545da874a42ec5de080ef7d0f
Michele <michele.pioli@studenti.unicam.it>
2022-04-12 11:05:48 +0200

    prettier

--- pages/board.js
+++ pages/board.js
@@ -1,44 +1,47 @@
-import Header from "../components/Header"
+import Header from "../components/Header";
 import InputMessage from "../components/InputMessage";
 import { supabase } from "../utils/supabase";
 import { BsSuitHeart } from "react-icons/bs";
 
 export default function board({ messages }) {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <Header />
-            <InputMessage />
-            <div className="w-full border-slate-300 rounded-xl max-w-4xl flex flex-col space-y-4">
-                {messages.map((message) => (
-                    <div className="rounded-xl m-8 p-8 md:p-4 shadow-sm shadow-slate-300" key={message.engagement}>
-                        <div className="flex justify-between h-24 rounded-full mx-auto">
-                            <span className="text-sm">Utente: {message.user_id}</span>
-                            <span className="text-sm">Interazioni: {message.engagement}</span>
-                        </div>
-                        <div className="text-center">
-                            <span className="text-lg font-bold">{message.title}</span>
-                        </div>
-                        <div className="flex flex-col items-center py-2">
-                            <p className="text-md font-thin">{message.body}</p>
-                            <button className="hover:text-pink-400 font-extrabold self-center">
-                                <BsSuitHeart />
-                            </button>
-                        </div>
-                    </div>
-                ))}
-            </div >
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Header />
+      <InputMessage />
+      <div className="w-full border-slate-300 rounded-xl max-w-4xl flex flex-col space-y-4">
+        {messages.map((message) => (
+          <div
+            className="rounded-xl m-8 p-8 md:p-4 shadow-sm shadow-slate-300"
+            key={message.engagement}
+          >
+            <div className="flex justify-between h-24 rounded-full mx-auto">
+              <span className="text-sm">Utente: {message.user_id}</span>
+              <span className="text-sm">Interazioni: {message.engagement}</span>
+            </div>
+            <div className="text-center">
+              <span className="text-lg font-bold">{message.title}</span>
+            </div>
+            <div className="flex flex-col items-center py-2">
+              <p className="text-md font-thin">{message.body}</p>
+              <button className="hover:text-pink-400 font-extrabold self-center">
+                <BsSuitHeart />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export const getServerSideProps = async () => {
-    const { data: messages } = await supabase.from("post").select("*")
+  const { data: messages } = await supabase.from("post").select("*");
 
-    const sortedMessages = messages.sort((a, b) => a.engagement + b.engagement);
+  const sortedMessages = messages.sort((a, b) => a.engagement + b.engagement);
 
-    return {
-        props: {
-            messages: sortedMessages,
-        }
-    }
-}
+  return {
+    props: {
+      messages: sortedMessages,
+    },
+  };
+};
