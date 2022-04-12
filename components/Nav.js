ff3e91a95eaec20ad7325d7be09b8c67ddb7d06c
ElGanzo <lorenzobensong@gmail.com>
2022-04-12 17:11:25 +0200

    minor fixes & fixes from DB update

--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import Link from "next/link";
 import { useUser } from "../context/user";
 
 export default function Nav() {
@@ -7,19 +7,19 @@ export default function Nav() {
   return (
     <nav className="py-4 px-6 border-b border-slate-300 text-sm font-medium">
       <ul className="flex space-x-3">
-        <Link href={user ? '/board' : '/'}>
+        <Link href={user ? "/board" : "/"}>
           <a>Home</a>
         </Link>
         <Link href="/pricing">
           <a>{user ? "Pagamenti" : ""}</a>
         </Link>
-        <Link href={'/contact-admin'}>
+        <Link href={"/contact-admin"}>
           <a className="">{"Contattaci"}</a>
-        </Link> 
-        <Link href={user ? '/logout' : '/login'}>
+        </Link>
+        <Link href={user ? "/logout" : "/login"}>
           <a className="">{user ? "Esci" : "Accedi"}</a>
         </Link>
       </ul>
     </nav>
-  )
+  );
 }
--- pages/board.js
+++ pages/board.js
@@ -2,23 +2,23 @@ import Header from "../components/Header"
 import { supabase } from "../utils/supabase";
 import { BsSuitHeart } from "react-icons/bs";
 
-export default function board({ messages }) {
-    console.log(messages);
+export default function board({ posts }) {
+    console.log(posts);
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <Header />
             <div className="w-full border-2 border-slate-300 rounded-xl max-w-4xl flex flex-col space-y-4">
-                {messages.map((message) => (
-                    <div className="rounded-xl m-8 p-8 md:p-4 shadow-sm shadow-slate-300" key={message.engagement}>
+                {posts.map((post) => (
+                    <div className="rounded-xl m-8 p-8 md:p-4 shadow-sm shadow-slate-300" key={post.engagement}>
                         <div className="flex justify-between h-24 rounded-full mx-auto">
-                            <span className="text-sm">Utente: {message.user_id}</span>
-                            <span className="text-sm">Interazioni: {message.engagement}</span>
+                            <span className="text-sm">Utente: {post.user_id}</span>
+                            <span className="text-sm">Interazioni: {post.engagement}</span>
                         </div>
                         <div className="text-center">
-                            <span className="text-lg font-bold">{message.title}</span>
+                            <span className="text-lg font-bold">{post.title}</span>
                         </div>
                         <div className="flex flex-col items-center py-2">
-                            <p className="text-md font-thin">{message.body}</p>
+                            <p className="text-md font-thin">{post.body}</p>
                             <button className="hover:text-pink-400 font-extrabold self-center">
                                 <BsSuitHeart />
                             </button>
@@ -31,13 +31,13 @@ export default function board({ messages }) {
 }
 
 export const getServerSideProps = async () => {
-    const { data: messages } = await supabase.from("board").select("*")
+    const { data: posts } = await supabase.from("post").select("*")
 
-    const sortedMessages = messages.sort((a, b) => a.engagement + b.engagement);
+    const sortedposts = posts.sort((a, b) => a.engagement + b.engagement);
 
     return {
         props: {
-            messages: sortedMessages,
+            posts: sortedposts,
         }
     }
 }
